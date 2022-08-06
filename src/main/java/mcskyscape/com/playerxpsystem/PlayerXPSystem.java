2d83d4dceaba1b899ad374b645c0b5ee7ac60280
Bailey Long <bailey-long@live.nmit.ac.nz>
2022-08-06 22:25:27 +1200

    Updated comments

--- src/main/java/mcskyscape/com/playerxpsystem/PlayerXPSystem.java
+++ src/main/java/mcskyscape/com/playerxpsystem/PlayerXPSystem.java
@@ -10,17 +10,18 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PlayerXPSystem extends JavaPlugin implements Listener {
+    //initialize HashMap
     public HashMap<Player, Integer> map = new HashMap<>();
-
+    //Setup event listener
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
     }
-
+    //Apply HashMap to Player
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         map.put(e.getPlayer(), 0);
     }
-
+    // When a player runs a command, increment Woodcutting and display it to player
     @EventHandler
     public void onCmd(PlayerCommandPreprocessEvent e) {
         int Woodcutting = map.get(e.getPlayer());
