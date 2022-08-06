f9a764681dd7bb3840678618615e1519a8180805
Bailey Long <bailey-long@live.nmit.ac.nz>
2022-08-07 01:49:34 +1200

    Initial commit

--- src/main/java/mcskyscape/com/playerxpsystem/PlayerXPSystem.java
+++ src/main/java/mcskyscape/com/playerxpsystem/PlayerXPSystem.java
@@ -1,31 +1,41 @@
 package mcskyscape.com.playerxpsystem;
 
 import java.util.HashMap;
+import java.util.Map;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlayerXPSystem extends JavaPlugin implements Listener {
-    //initialize HashMap
-    public HashMap<Player, Integer> map = new HashMap<>();
-    //Setup event listener
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+public class PlayerXPSystem extends JavaPlugin implements CommandExecutor {
+    @Override
+    public void onEnable(){
+        this.getCommand("levels").setExecutor(new Commandseelevels());
     }
-    //Apply HashMap to Player
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        map.put(e.getPlayer(), 0);
+    @Override
+    public void onDisable(){
+        
     }
-    // When a player runs a command, increment Woodcutting and display it to player
-    @EventHandler
-    public void onCmd(PlayerCommandPreprocessEvent e) {
-        int Woodcutting = map.get(e.getPlayer());
-        map.replace(e.getPlayer(), Woodcutting + 1);
-        e.getPlayer().sendMessage("Woodcutting = " + Woodcutting);
+
+    public class Commandseelevels implements CommandExecutor {
+        @Override
+        public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+            if (label.equals("levels")){
+                Player player = (Player) sender;
+                if (sender instanceof Player) {
+                    player.sendMessage("Your woodcutting is 0");
+                    player.sendMessage("Your Mining is 0");
+                    player.sendMessage("Your Combat is 0");
+                }
+                return true;
+            }
+            return false;
+        }
     }
+
+
 }
